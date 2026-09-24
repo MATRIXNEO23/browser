c04800105b879b6dec685dd9c21de4a624098cf1
MATRIXNEO23 <101059510+MATRIXNEO23@users.noreply.github.com>
2026-09-24 15:51:30 +0200

    add profiles/templates/ghost/user.js

---
+++ profiles/templates/ghost/user.js
@@ -0,0 +1,8 @@
+user_pref("browser.tabs.unloadOnLowMemory", true);
+user_pref("browser.privatebrowsing.autostart", true);
+user_pref("privacy.resistFingerprinting", true);
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("privacy.trackingprotection.fingerprinting.enabled", true);
+user_pref("privacy.trackingprotection.cryptomining.enabled", true);
+user_pref("network.cookie.cookieBehavior", 5);
+user_pref("media.peerconnection.enabled", false);
