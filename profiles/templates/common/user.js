8c9994089dd51a3a092bf3e9f7b24ab6103699ea
MATRIXNEO23 <101059510+MATRIXNEO23@users.noreply.github.com>
2026-09-24 18:48:55 +0200

    remove obsolete profile template profiles/templates/common/user.js

--- profiles/templates/common/user.js
+++
@@ -1,7 +0,0 @@
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("browser.toolbars.bookmarks.visibility", "always");
-user_pref("browser.uidensity", 1);
-
-// Development build only: allows the bundled internal control core before AMO signing.
-// Remove this preference from release builds after the internal XPI is signed.
-user_pref("xpinstall.signatures.required", false);
