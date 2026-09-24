8a743d267e99d423d838a124300f2642b5159f77
MATRIXNEO23 <101059510+MATRIXNEO23@users.noreply.github.com>
2026-09-24 16:25:52 +0200

    allow internal core on ESR development builds

--- profiles/templates/common/user.js
+++ profiles/templates/common/user.js
@@ -1,3 +1,7 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.toolbars.bookmarks.visibility", "always");
 user_pref("browser.uidensity", 1);
+
+// Development build only: allows the bundled internal control core before AMO signing.
+// Remove this preference from release builds after the internal XPI is signed.
+user_pref("xpinstall.signatures.required", false);
