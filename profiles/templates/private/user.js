069eddc61471417a1b7072856ca9af2f6efba48f
MATRIXNEO23 <101059510+MATRIXNEO23@users.noreply.github.com>
2026-09-24 18:49:19 +0200

    remove obsolete profile template profiles/templates/private/user.js

--- profiles/templates/private/user.js
+++
@@ -1,7 +0,0 @@
-user_pref("browser.tabs.unloadOnLowMemory", true);
-user_pref("privacy.resistFingerprinting", false);
-user_pref("privacy.trackingprotection.enabled", true);
-user_pref("privacy.trackingprotection.socialtracking.enabled", true);
-user_pref("privacy.trackingprotection.fingerprinting.enabled", true);
-user_pref("privacy.trackingprotection.cryptomining.enabled", true);
-user_pref("network.cookie.cookieBehavior", 5);
