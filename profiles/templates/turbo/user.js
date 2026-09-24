10787083396d66359f5cdc56821216b0023feb73
MATRIXNEO23 <101059510+MATRIXNEO23@users.noreply.github.com>
2026-09-24 18:49:07 +0200

    remove obsolete profile template profiles/templates/turbo/user.js

--- profiles/templates/turbo/user.js
+++
@@ -1,6 +0,0 @@
-user_pref("browser.tabs.unloadOnLowMemory", true);
-user_pref("privacy.resistFingerprinting", false);
-user_pref("media.autoplay.default", 5);
-user_pref("network.prefetch-next", false);
-user_pref("network.dns.disablePrefetch", true);
-user_pref("network.http.speculative-parallel-limit", 0);
