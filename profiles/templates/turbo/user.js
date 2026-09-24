436d9281d7832012b33da932d8ffc16ea6332c75
MATRIXNEO23 <101059510+MATRIXNEO23@users.noreply.github.com>
2026-09-24 15:51:25 +0200

    add profiles/templates/turbo/user.js

---
+++ profiles/templates/turbo/user.js
@@ -0,0 +1,6 @@
+user_pref("browser.tabs.unloadOnLowMemory", true);
+user_pref("privacy.resistFingerprinting", false);
+user_pref("media.autoplay.default", 5);
+user_pref("network.prefetch-next", false);
+user_pref("network.dns.disablePrefetch", true);
+user_pref("network.http.speculative-parallel-limit", 0);
